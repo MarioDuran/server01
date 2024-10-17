185b5bb8f116dfefdf3fb71f60b227ce4bff51fc
Mario Duran-Vega <57765660+MarioDuran@users.noreply.github.com>
2024-10-17 17:52:14 -0600

    Server Demo 01

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+const port = 3000
+app.listen(port)
+console.log('server on port', port)
